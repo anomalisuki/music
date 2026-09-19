db75e222829a8289ceffb35307c01389ef243084
anomalisuki <davied2603@gmail.com>
2026-09-19 19:43:47 +0700

    Upload via up2git

--- miniplayer.js
+++ miniplayer.js
@@ -270,6 +270,6 @@ var MP={
             }
         };
         img.onerror = function() {}; // silently keep the hash fallback if the image can't be read
-        img.src = 'https://nanzz-music.netlify.app/api/proxy-image?url=' + encodeURIComponent(track.cover);
+        img.src = '/api/nanzz/proxy-image?url=' + encodeURIComponent(track.cover);
     }
 };
